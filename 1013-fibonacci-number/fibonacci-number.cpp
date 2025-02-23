7ea0ab56ec0aab1f0d87c29f931c73bd7b839b4a
Madhav <madhavguglani@gmail.com>
2025-02-23 21:20:16 +0530

    Time: 14 ms (22.90%) | Memory: 7.6 MB (98.36%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,14 +1,10 @@
 class Solution {
 public:
-    int fib(int n, vector<int>& dp) {
-        if(n <= 1) return n;  // Base Case
-        if(dp[n] != -1) return dp[n];  // If already computed, return it
-
-        return dp[n] = fib(n-1, dp) + fib(n-2, dp); // Store result
-    }
-
     int fib(int n) {
-        vector<int> dp(n+1, -1);  // Create DP array
-        return fib(n, dp);
+        // Base cases
+        if (n <= 1) return n;
+        
+        // Recursive calculation
+        return fib(n - 1) + fib(n - 2);
     }
 };
